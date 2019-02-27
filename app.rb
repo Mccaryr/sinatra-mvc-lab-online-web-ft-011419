87c89e74e026c71f18ae02cfb54079eb738c8982
Robert McCary <robert.mccary@yahoo.com>
2019-02-27 18:45:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  # post '/piglatinize' do
-  # @piglatinized_text = Piglatinizer.new(params[:user_phrase])
-  # erb :results
-  # end
+  post '/piglatinize' do
+  @piglatinized_text = Piglatinizer.new(params[:user_phrase])
+  erb :results
+  end
 end
