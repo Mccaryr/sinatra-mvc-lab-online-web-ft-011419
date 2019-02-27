1cd9c18445661f05cbcb3ec388f06040934c55f6
Robert McCary <robert.mccary@yahoo.com>
2019-02-27 18:43:22 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,23 +6,23 @@ class PigLatinizer
      input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
 
-  private 
+  private
 
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
-  end 
+  end
 
   def piglatinize_word(word)
-  
+
     if !consonant?(word[0])
       word = word + "w"
 
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-  
+
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
-  
+
     else
       word = word.slice(1..-1) + word.slice(0)
     end
